78d55c607bd64a06810aa6caf42dd52e8a22bf34
Rafael <rafuk89@gmail.com>
2016-01-04 17:49:36 +0100

    minor changes in style on team site

--- team.php
+++ team.php
@@ -25,6 +25,7 @@
 <div class="banner nomarginbanner">
   <div class="container text-center">
     <h2><?php echo $name ?></h2>
+
     <p><?php echo $info ?></p>
   </div>
 </div>
@@ -32,14 +33,16 @@
 <div class="container">
   <div class="row img-bck-1" style="background-image: url('<?php echo $img2 ?>')">
     <div class="col-md-6 col-md-offset-6">
-      <p>
+      <h2 class="team-h2"><?php echo $h2 ?></h2>
+      <h3 class="team-h3"><?php echo $h3 ?></h3>
+      <p class="team-p">
         <?php echo $text1 ?>
       </p>
     </div>
   </div>
   <div class="row img-bck-2" style="background-image: url('<?php echo $img3 ?>')">
     <div class="col-md-6">
-      <p>
+      <p class="team-p">
         <?php echo $text2 ?>
       </p>
     </div>
