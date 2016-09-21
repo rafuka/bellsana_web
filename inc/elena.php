5991f4fdcbe2eeebf5a6c597d6fa0f9077c8a3be
J. Rafael Garcia <rafuk89@gmail.com>
2016-09-21 14:18:17 +0200

    fixed umlauds

--- inc/elena.php
+++ inc/elena.php
@@ -16,25 +16,25 @@
 
 
   $text1 = "<h3 class='text-p'></h3>
-  <p class='text-p'><br/>Unser Gesicht spiegelt immer unseren geistigen Zustand. Stress und Emotionen lassen sich daraus ablesen. Die Mimik ist ein wesentlicher Teil unserer Aura und verrät viel über unsere Persönlichkeit.</p>
+  <p class='text-p'><br/>Unser Gesicht spiegelt immer unseren geistigen Zustand. Stress und Emotionen lassen sich daraus ablesen. Die Mimik ist ein wesentlicher Teil unserer Aura und verr&auml;t viel &uuml;ber unsere Pers&ouml;nlichkeit.</p>
 
-  <p class='text-p'>Gönnen Sie sich die einzigartige Gesichtsmassage, die den Ausdruck von Müdigkeit und Stress verschwinden lässt. Die Qualität und der Teint Ihres Gesichts wird auf ungeahnte Weise verbessert; Sie sehen jünger aus. Die positive Wirkung zeigt sich bereits nach der ersten Behandlung. Dies ist das natürliche Anti-Aging.</p>
+  <p class='text-p'>G&ouml;nnen Sie sich die einzigartige Gesichtsmassage, die den Ausdruck von M&uuml;digkeit und Stress verschwinden l&auml;sst. Die Qualit&auml;t und der Teint Ihres Gesichts wird auf ungeahnte Weise verbessert; Sie sehen j&uuml;nger aus. Die positive Wirkung zeigt sich bereits nach der ersten Behandlung. Dies ist das nat&uuml;rliche Anti-Aging.</p>
 
   <p class='text-p'>Ich liebe meinen Job!</p>
 
 	<h3 class='text-p'>Gesichtmassage</h3>
 
-  <p class='text-p'>Die plastische, bukkale Gesichtstiefenmassage ist eine Straffungsmethode, um das Gesicht zu heben, die Gesichtsmuskeln zu stärken, die Form zu korrigieren und unschönes Absacken zu reduzieren. Das Verfahren konzentriert sich auf die Arbeit mit den drei skulpturalen Linien des Gesichts:</p>
+  <p class='text-p'>Die plastische, bukkale Gesichtstiefenmassage ist eine Straffungsmethode, um das Gesicht zu heben, die Gesichtsmuskeln zu st&auml;rken, die Form zu korrigieren und unsch&ouml;nes Absacken zu reduzieren. Das Verfahren konzentriert sich auf die Arbeit mit den drei skulpturalen Linien des Gesichts:</p>
 
   <ul class='text-p'>
-    <li>Plastische Kieferlinie (hebt schlaffe Wangen, bringt das Kinn in Übereinstimmung, erzielt gerade Linien);</li>
+    <li>Plastische Kieferlinie (hebt schlaffe Wangen, bringt das Kinn in &uuml;bereinstimmung, erzielt gerade Linien);</li>
     <li>Plastische Jochbeinlinien (vermindern das Aufquellen des Gesichts, verdeutlichen die Gesichtskonturen, heben das Relief des Jochbeins hervor);</li>
     <li>Plastische Augenbrauenlinien (straffen und anheben der Augenlider) </li>
   </ul>
 
-	<p class='text-p'>Der Schlüssel zum Erfolg dieser Methoden liegt in der gleichzeitigen Fokussierung auf zwei verschiedene Phasen: plastische und Tiefengewebe. Dieser Ansatz korrigiert die Haltung des Gesichts, hilft ihm „aufzublühen“, seine Schönheit wiederzuerlangen und unterstützt das Gesicht, eine gesunde Gesichtsfarbe mit elastischer Haut und ohne Falten zu entwickeln.</p>
+	<p class='text-p'>Der Schl&uuml;ssel zum Erfolg dieser Methoden liegt in der gleichzeitigen Fokussierung auf zwei verschiedene Phasen: plastische und Tiefengewebe. Dieser Ansatz korrigiert die Haltung des Gesichts, hilft ihm „aufzubl&uuml;hen“, seine Sch&ouml;nheit wiederzuerlangen und unterst&uuml;tzt das Gesicht, eine gesunde Gesichtsfarbe mit elastischer Haut und ohne Falten zu entwickeln.</p>
 
-  <p class='text-p'>Während der plastischen Phase werden die Muskeln des Gesichts von aussen stimuliert und massiert, während in der Tiefengewebe Phase die Muskeln von innen durch die Mundhöhle massiert werden.</p>
+  <p class='text-p'>W&auml;hrend der plastischen Phase werden die Muskeln des Gesichts von aussen stimuliert und massiert, w&auml;hrend in der Tiefengewebe Phase die Muskeln von innen durch die Mundh&ouml;hle massiert werden.</p>
 
   <h3 class='text-p'>Ergebnis</h3>
 
@@ -48,22 +48,22 @@
 
   <p class='text-p'>Muskelentspannung, eine allgemeine Verbesserung des Muskeltonus, des Lymphflusses, der Mikrozirkulation des subkutanen Fettgewebes und der Durchblutung, Beseitigung einer Ptosis.</p>
 
-  <p class='text-p'>Die Kombination dieser Techniken führt zu einem wirksamen nicht-chirurgische Facelifting und was noch viel wichtiger ist, es ist ein natürlicher Prozess, welcher der Tendenz plastischer Operationen entgegenwirkt, ein neues Gesicht zu ‘skulpturalen’ und die natürlichen Verjüngungsmechanismen zu ignorieren.</p>
+  <p class='text-p'>Die Kombination dieser Techniken f&uuml;hrt zu einem wirksamen nicht-chirurgische Facelifting und was noch viel wichtiger ist, es ist ein nat&uuml;rlicher Prozess, welcher der Tendenz plastischer Operationen entgegenwirkt, ein neues Gesicht zu ‘skulpturalen’ und die nat&uuml;rlichen Verj&uuml;ngungsmechanismen zu ignorieren.</p>
 
-  <p class='text-p'>Darüber hinaus ist es bemerkenswert, dass die plastische, bukkale Tiefengewebemassage eine weitere Funktion hat: Neben dem Hauptziel des Faceliftings und der Muskelstärkung ist diese Technik darauf ausgerichtet, Ihr Gesicht mit Ihren emotionalen Zustand zu verbinden, sowie Blockierungen, Stress und Angst zu beseitigen.</p>
+  <p class='text-p'>Dar&uuml;ber hinaus ist es bemerkenswert, dass die plastische, bukkale Tiefengewebemassage eine weitere Funktion hat: Neben dem Hauptziel des Faceliftings und der Muskelst&auml;rkung ist diese Technik darauf ausgerichtet, Ihr Gesicht mit Ihren emotionalen Zustand zu verbinden, sowie Blockierungen, Stress und Angst zu beseitigen.</p>
 
-  <p class='text-p'>Es kann schwierig sein zu glauben, aber unsere alltäglichen Sorgen wirken sich auf den Zustand unseres Gesichts und unsere Haut aus. Diese exklusive Methode wird ihr Bestes dazu beitragen, damit Sie nicht nur jünger und schöner ausschauen, sondern auch unnötigen Stress abschütteln können und letztlich zu mehr Ruhe und innerer Harmonie finden.</p>
+  <p class='text-p'>Es kann schwierig sein zu glauben, aber unsere allt&auml;glichen Sorgen wirken sich auf den Zustand unseres Gesichts und unsere Haut aus. Diese exklusive Methode wird ihr Bestes dazu beitragen, damit Sie nicht nur j&uuml;nger und sch&ouml;ner ausschauen, sondern auch unn&ouml;tigen Stress absch&uuml;tteln k&ouml;nnen und letztlich zu mehr Ruhe und innerer Harmonie finden.</p>
 
   <h3 class='text-p'>Gegenanzeigen</h3>
 
-  <p class='text-p'>Es ist wichtig zu beachten, dass es Gegenanzeigen für die Verwendung von Plastifizieren Massage sind. Wir empfehlen nicht, diese Verfahren in den folgenden Fällen durchgeführt:</p>
+  <p class='text-p'>Es ist wichtig zu beachten, dass es Gegenanzeigen f&uuml;r die Verwendung von Plastifizieren Massage sind. Wir empfehlen nicht, diese Verfahren in den folgenden F&auml;llen durchgef&uuml;hrt:</p>
 
   <ul class='text-p'>
-    <li>Bei Erkrankungen der Schilddrüse – wegen der Gefahr einer Exazerbation. In diesem Fall wird die Entscheidung über die Möglichkeit der skulpturalen Massage Endokrinologen genommen;</li>
+    <li>Bei Erkrankungen der Schilddr&uuml;se – wegen der Gefahr einer Exazerbation. In diesem Fall wird die Entscheidung &uuml;ber die M&ouml;glichkeit der skulpturalen Massage Endokrinologen genommen;</li>
     <li>In der Gegenwart von Krebs, insbesondere der Haut;</li>
-    <li>Mit verschiedenen Hautausschläge, Hauterkrankungen, akuten Herpes;</li>
-    <li>Im Fall von Infektionskrankheiten, Verschlimmerung der chronischen Erkrankung bei einer erhöhten Temperatur;</li>
-    <li>Wenn es mechanische Beschädigungen der Haut - Wunden und Kratzer.</li>
+    <li>Mit verschiedenen Hautausschl&auml;ge, Hauterkrankungen, akuten Herpes;</li>
+    <li>Im Fall von Infektionskrankheiten, Verschlimmerung der chronischen Erkrankung bei einer erh&ouml;hten Temperatur;</li>
+    <li>Wenn es mechanische Besch&auml;digungen der Haut - Wunden und Kratzer gibt.</li>
   </ul>
     ";
 ?>
